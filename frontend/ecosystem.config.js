bbbe7e9fa56a8758952499d601ac15ffd50ac79d
kuzinvm <kuzinvm@kuzinvm-ubuntu-20-04.ru-central1.internal>
2023-04-05 17:35:04 +0000

    fix: fix post-deploy script

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: "https://github.com/kuzinartemiy/web-plus-pm2-deploy.git",
       path: DEPLOY_PATH,
-      "post-deploy": "npm i && run build",
+      "post-deploy": "npm i && npm run build",
     },
   },
 };
