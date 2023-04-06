4dfbb66a25b9b4aef93e3a6e2a08157c3a2dbf48
kuzinvm <kuzinvm@kuzinvm-ubuntu-20-04.ru-central1.internal>
2023-04-06 16:48:38 +0000

    fix: update backend ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'post-deploy': 'npm i && npm run build && pm2 restart api-service --update-env',
     },
   },
 };
